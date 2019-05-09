5eff2a6316654b1c16efc2a0b95705fdc258a375
jscholar <jinruncai@gmail.com>
2019-05-08 17:01:14 -0700

    Delegated prop update to click event.

--- src/Containers/Pendings/SpecMenu/SpecLink/SpecLink.js
+++ src/Containers/Pendings/SpecMenu/SpecLink/SpecLink.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import WithStatusClass from './../../../../hoc/WithStatusClass/WithStatusClass';
-import classes from './SpecLink.module.css'
+import classes from './SpecLink.module.css';
+
+import { fetchSpec } from '../../../../Database/database';
 
 const SpecLink = (props) => (
-    <div className={classes.SpecLink}>
+    <div onClick={() => fetchSpec(props.spec)} className={classes.SpecLink}>
         <WithStatusClass status={props.status}>
             <NavLink
                 activeClassName={classes.Test}
